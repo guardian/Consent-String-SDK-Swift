2d7822fb740966ba77a6f0ffc0853287eda4fb72
Alexander Edge <alex@alexedge.co.uk>
2019-05-22 11:35:42 +0100

    Add VendorIdentifier typealias

---
+++ Consent String SDK Swift/VendorIdentifier.swift
@@ -0,0 +1,11 @@
+//
+//  VendorIdentifier.swift
+//  Consent String SDK Swift
+//
+//  Created by Alexander Edge on 22/05/2019.
+//  Copyright © 2019 Interactive Advertising Bureau. All rights reserved.
+//
+
+import Foundation
+
+public typealias VendorIdentifier = Int16
