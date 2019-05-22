3a848dbd05ea4ee7107209863f253646093dbf9b
Alexander Edge <alex@alexedge.co.uk>
2019-05-22 10:02:13 +0100

    Use setUp / tearDown to create builder

--- Consent String SDK SwiftTests/ConsentStringBuilderTests.swift
+++ Consent String SDK SwiftTests/ConsentStringBuilderTests.swift
@@ -11,58 +11,53 @@ import XCTest
 
 class ConsentStringBuilderTests: XCTestCase, BinaryStringTestSupport {
 
+    var builder: ConsentStringBuilder!
+
     override func setUp() {
         super.setUp()
+        builder = ConsentStringBuilder()
     }
 
     override func tearDown() {
+        builder = nil
         super.tearDown()
     }
 
     func testEncodingInt() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(integer: 1, toLength: NSRange.version.length), "000001")
     }
 
     func testEncodingDate() {
-        let builder = ConsentStringBuilder()
         let date = Date(timeIntervalSince1970: 1510082155.4)
         XCTAssertEqual(builder.encode(date: date, toLength: NSRange.updated.length), "001110000100000101000100000000110010")
     }
 
     func testEncodingBitfield() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(vendorBitFieldForVendors: [2,4,6,8,10,12,14,16,18,20], maxVendorId: 20), "01010101010101010101")
     }
 
     func testEncodingNoVendorRanges() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(vendorRanges: []), "000000000000")
     }
 
     func testEncodingSingleVendorIdRange() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(vendorRanges: [9...9]), "00000000000100000000000001001")
     }
 
     func testEncodingMultipleVendorIdRange() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(vendorRanges: [1...3]), "000000000001100000000000000010000000000000011")
     }
 
     func testEncodingMixedVendorRanges() {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(builder.encode(vendorRanges: [1...3, 9...9]), "00000000001010000000000000001000000000000001100000000000001001")
     }
 
     func testUsesRangesOverBitField() throws {
-        let builder = ConsentStringBuilder()
         XCTAssertEqual(try builder.build(created: Date(timeIntervalSince1970: 1510082155.4), updated: Date(timeIntervalSince1970: 1510082155.4), cmpId: 7, cmpVersion: 1, consentScreenId: 3, consentLanguage: "EN", allowedPurposes: [1,2,3], vendorListVersion: 8, maxVendorId: 2011, defaultConsent: true, allowedVendorIds: Set(1...2011).subtracting([9])), "BOEFEAyOEFEAyAHABDENAI4AAAB9vABAASA")
     }
 
     func testUsesBitFieldOverRanges() throws {
         let vendorIds = (1...234).compactMap { $0.isMultiple(of: 2) ? nil : $0 }
-        let builder = ConsentStringBuilder()
          XCTAssertEqual(try builder.build(created: Date(timeIntervalSince1970: 1510082155.4), updated: Date(timeIntervalSince1970: 1510082155.4), cmpId: 7, cmpVersion: 1, consentScreenId: 3, consentLanguage: "EN", allowedPurposes: [1,2,3], vendorListVersion: 8, maxVendorId: 2011, defaultConsent: true, allowedVendorIds: Set(vendorIds)), "BOEFEAyOEFEAyAHABDENAI4AAAB9tVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     }
 }
